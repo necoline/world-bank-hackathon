bc599f7e467d2e8aeac9b782a54ebb515746a1a1
Brennan-Young <m.brennanyoung@gmail.com>
2017-10-21 11:07:36 -0700

    spark hello world

---
+++ womenWhoCode/world-bank/analytics/build.sbt
@@ -0,0 +1,7 @@
+name := "Poor Ratio Calculations"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.1.1"
---
+++ womenWhoCode/world-bank/analytics/src/main/scala/AnalyticsMain.scala
@@ -0,0 +1,12 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import java.io._
+
+
+object Analytics{
+  
+  def main(args: Array[String]){
+    println("hello world")
+  }
+}
