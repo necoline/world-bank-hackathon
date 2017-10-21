4ebb610c6b063d4ecf1460c691d78e34a63effb0
necoline <hnhubner@gmail.com>
2017-10-21 14:51:15 -0700

    simple chart working

--- womenWhoCode/world-bank/src/App.js
+++ womenWhoCode/world-bank/src/App.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import SideMenu from './SideMenu.js';
 import Graph from './Graph.js';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      chartData: {},
+    };
+  }
+
+  componentWillMount() {
+    this.getChartData();
+  }
+
+  getChartData() {
+    // Ajax calls here
+    this.setState({
+      chartData: {
+        labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
+        datasets: [
+          {
+            label: 'Population',
+            data: [617594, 181045, 153060, 106519, 105162, 95072],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              'rgba(153, 102, 255, 0.6)',
+              'rgba(255, 159, 64, 0.6)',
+              'rgba(255, 99, 132, 0.6)',
+            ],
+          },
+        ],
+      },
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -15,7 +49,7 @@ class App extends Component {
           <SideMenu />
         </div>
         <div>
-          <Graph />
+          <Graph chartData={this.state.chartData} location="Massachusetts" legendPosition="bottom" />
         </div>
       </div>
     );
--- womenWhoCode/world-bank/src/Graph.js
+++ womenWhoCode/world-bank/src/Graph.js
@@ -1,11 +1,69 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Bar, Line, Pie } from 'react-chartjs-2';
 
 class Graph extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chartData: props.chartData,
+    };
+  }
+
+  static defaultProps = {
+    displayTitle: true,
+    displayLegend: true,
+    legendPosition: 'right',
+    location: 'City',
+  };
+
   render() {
     return (
-      <div>
-        Graph
+      <div className="chart">
+        <Bar
+          data={this.state.chartData}
+          options={{
+            title: {
+              display: this.props.displayTitle,
+              text: 'Largest Cities In ' + this.props.location,
+              fontSize: 25,
+            },
+            legend: {
+              display: this.props.displayLegend,
+              position: this.props.legendPosition,
+            },
+          }}
+        />
+
+        <Line
+          data={this.state.chartData}
+          options={{
+            title: {
+              display: this.props.displayTitle,
+              text: 'Largest Cities In ' + this.props.location,
+              fontSize: 25,
+            },
+            legend: {
+              display: this.props.displayLegend,
+              position: this.props.legendPosition,
+            },
+          }}
+        />
+
+        <Pie
+          data={this.state.chartData}
+          options={{
+            title: {
+              display: this.props.displayTitle,
+              text: 'Largest Cities In ' + this.props.location,
+              fontSize: 25,
+            },
+            legend: {
+              display: this.props.displayLegend,
+              position: this.props.legendPosition,
+            },
+          }}
+        />
       </div>
     );
   }
